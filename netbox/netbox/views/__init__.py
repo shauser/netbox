12d6ec67d5efca45f3d84158c7a918d6523024f2
Arthur <worldnomad@gmail.com>
2022-09-19 14:24:13 -0700

    7016 add search indexes

--- netbox/netbox/views/__init__.py
+++ netbox/netbox/views/__init__.py
@@ -157,15 +157,15 @@ class SearchView(View):
             # If an object type has been specified, redirect to the dedicated view for it
             if form.cleaned_data['obj_type']:
                 object_type = form.cleaned_data['obj_type']
-                url = reverse(SEARCH_TYPES[object_type]['url'])
+                url = reverse(SEARCH_TYPES[object_type].url)
                 return redirect(f"{url}?q={form.cleaned_data['q']}")
 
             for obj_type in SEARCH_TYPES.keys():
 
-                queryset = SEARCH_TYPES[obj_type]['queryset'].restrict(request.user, 'view')
-                filterset = SEARCH_TYPES[obj_type]['filterset']
-                table = SEARCH_TYPES[obj_type]['table']
-                url = SEARCH_TYPES[obj_type]['url']
+                queryset = SEARCH_TYPES[obj_type].queryset.restrict(request.user, 'view')
+                filterset = SEARCH_TYPES[obj_type].filterset
+                table = SEARCH_TYPES[obj_type].table
+                url = SEARCH_TYPES[obj_type].url
 
                 # Construct the results table for this object type
                 filtered_queryset = filterset({'q': form.cleaned_data['q']}, queryset=queryset).qs
